048f6ab59b97765c45a1a446cf8e300e7b7b8a3f
NisrinMl <www.niso2021@gmail.com>
2024-07-18 15:10:11 +0300

    MainPage Endpoint * add Tours Component

--- src/features/user/userView/MainPage.jsx
+++ src/features/user/userView/MainPage.jsx
@@ -2,13 +2,14 @@ import AboutUs from "./MainPageComponent/AboutUs"
 import ContactUs from "./MainPageComponent/ContactUs"
 import HelpCenter from "./MainPageComponent/HelpCenter"
 import OurPrivacy from "./MainPageComponent/OurPrivacy"
+import Tours from "./MainPageComponent/Tours"
 import WhatWeCanDo from "./MainPageComponent/WhatWeCanDo"
 
 function MainPage() {
     return (
         <div className="flex flex-col space-y-14 overflow-x-hidden">
 
-
+            <Tours/>
             <WhatWeCanDo />
             <AboutUs />
             <OurPrivacy />
